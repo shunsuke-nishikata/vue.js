b72b2f38baea8b0fc120b4af4708a302ff45cb08
shunsuke-nishikata <1037shun@gmail.com>
2020-12-06 18:55:04 +0900

    todoリスト作成

--- index.js
+++ index.js
@@ -2,5 +2,29 @@ const vm = new Vue({
   el: '#app',
   data: {
     message: 'Hello Vue.js!',
+    isChecked: true
+  },
+})
+
+const vm1 = new Vue({
+  el: '#list',
+  data: {
+    items: [
+        { title: '項目1', id: 1 },
+        { title: '項目2', id: 2 },
+        { title: '項目3', id: 3 },
+    ]
   }
+})
+
+const vm2 = new Vue ({
+  el: '#ev',
+  data: {
+    counter: 0
+  },
+  methods: {
+    add: function() {
+      this.counter++
+    },
+  },
 })
